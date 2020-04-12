14f39dc423c7a5c0be2ee4b9c449553b7b7a5cd5
shininghyunho <46080945+shininghyunho@users.noreply.github.com>
2020-04-12 23:06:37 +0900

    revised the basics

--- app/src/main/java/com/example/my_yolo/AttatchmentActivity.kt
+++ app/src/main/java/com/example/my_yolo/AttatchmentActivity.kt
@@ -1,5 +1,6 @@
 package com.example.my_yolo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,9 @@ class AttatchmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_attatchment)
+
+        val get_intent = Intent(Intent.ACTION_GET_CONTENT)
+        get_intent.setType("*/*")
+        startActivityForResult(get_intent,10)
     }
 }
