17a12187ab4be455a8daa63c94da296bddd7ffd3
Woobin <wbjung2917@naver.com>
2020-04-26 18:41:55 +0900

    Move permission to LaunchActivity

--- app/src/main/java/com/example/my_yolo/LaunchActivity.kt
+++ app/src/main/java/com/example/my_yolo/LaunchActivity.kt
@@ -1,11 +1,47 @@
 package com.example.my_yolo
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_launch.*
 
 class LaunchActivity : AppCompatActivity() {
+
+    val PERMISSONS_REQUEST_CODE=100;
+    var REQUIRE_PERMISSIONS=arrayOf(Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    private fun permissionCheck(){
+        var cameraPermission : Int= ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        var writeExternalStragePermission : Int= ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if(cameraPermission== PackageManager.PERMISSION_GRANTED && writeExternalStragePermission== PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this,"permissions granted", Toast.LENGTH_LONG).show()
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this,REQUIRE_PERMISSIONS,PERMISSONS_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==PERMISSONS_REQUEST_CODE && grantResults[0]== PackageManager.PERMISSION_GRANTED&&grantResults[1]== PackageManager.PERMISSION_GRANTED)
+        {
+            Toast.makeText(this,"permissions granted", Toast.LENGTH_LONG).show()
+        }
+        else
+        {
+            Toast.makeText(this,"permissions are not granted", Toast.LENGTH_LONG).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
